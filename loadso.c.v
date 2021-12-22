85c5b962ddd2bd4f1166e77ff049ef1ca25f4ad0
lmp <dev.larpon@gmail.com>
2021-12-22 13:20:23 +0100

    loadso: fic docs

--- loadso.c.v
+++ loadso.c.v
@@ -6,31 +6,27 @@ module sdl
 //
 // SDL_loadso.h
 //
-
-/**
- *  This function dynamically loads a shared object and returns a pointer
- *  to the object handle (or NULL if there was an error).
- *  The 'sofile' parameter is a system dependent name of the object file.
-*/
 fn C.SDL_LoadObject(sofile &char) voidptr
+
+// load_object dynamically loads a shared object and returns a pointer
+// to the object handle (or NULL if there was an error).
+// The 'sofile' parameter is a system dependent name of the object file.
 pub fn load_object(sofile string) voidptr {
 	return C.SDL_LoadObject(sofile.str)
 }
 
-/**
- *  Given an object handle, this function looks up the address of the
- *  named function in the shared object and returns it.  This address
- *  is no longer valid after calling SDL_UnloadObject().
-*/
 fn C.SDL_LoadFunction(handle voidptr, name &char) voidptr
+
+// load_function, given an object handle, looks up the address of the
+// named function in the shared object and returns it.  This address
+// is no longer valid after calling SDL_UnloadObject().
 pub fn load_function(handle voidptr, name string) voidptr {
 	return C.SDL_LoadFunction(handle, name.str)
 }
 
-/**
- *  Unload a shared object from memory.
-*/
 fn C.SDL_UnloadObject(handle voidptr)
+
+// unload_object unloads a shared object from memory.
 pub fn unload_object(handle voidptr) {
 	C.SDL_UnloadObject(handle)
 }
