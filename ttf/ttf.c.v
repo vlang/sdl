33080ff70209faeea251bc4b967cb2cc674bc776
Larpon <Larpon@users.noreply.github.com>
2021-12-26 14:57:44 +0100

    ttf: fix docs (#51)

--- ttf/ttf.c.v
+++ ttf/ttf.c.v
@@ -29,33 +29,35 @@ pub const (
 	unicode_bom_swapped = C.UNICODE_BOM_SWAPPED // 0xFFFE
 )
 
-// This function tells the library whether UNICODE text is generally
+fn C.TTF_ByteSwappedUNICODE(swapped int)
+
+// byte_swapped_unicode is a function that tells the library whether UNICODE text is generally
 // byteswapped.  A UNICODE BOM character in a string will override
 // this setting for the remainder of that string.
-fn C.TTF_ByteSwappedUNICODE(swapped int)
 pub fn byte_swapped_unicode(swapped int) {
 	C.TTF_ByteSwappedUNICODE(swapped)
 }
 
-// The internal structure containing font information
+// Font is the internal structure containing font information
+// Font is C.TTF_Font
 [typedef]
 struct C.TTF_Font {
 }
 
 pub type Font = C.TTF_Font
 
-// Initialize the TTF engine - returns 0 if successful, -1 on error
+// init initializes the TTF engine - returns 0 if successful, -1 on error
 fn C.TTF_Init() int
 pub fn init() int {
 	return C.TTF_Init()
 }
 
-/*
-Open a font file and create a font of the specified point size.
- * Some .fon fonts will have several sizes embedded in the file, so the
- * point size becomes the index of choosing which size.  If the value
- * is too high, the last indexed size will be the default.*/
 fn C.TTF_OpenFont(file &char, ptsize int) &C.TTF_Font
+
+// open_font opens a font file and create a font of the specified point size.
+// Some .fon fonts will have several sizes embedded in the file, so the
+// point size becomes the index of choosing which size.  If the value
+// is too high, the last indexed size will be the default.*/
 pub fn open_font(file &char, ptsize int) &Font {
 	return C.TTF_OpenFont(file, ptsize)
 }
@@ -122,51 +124,60 @@ pub fn set_font_hinting(font &Font, hinting int) {
 	C.TTF_SetFontHinting(font, hinting)
 }
 
-// Get the total height of the font - usually equal to point size
 fn C.TTF_FontHeight(font &C.TTF_Font) int
+
+// font_height gets the total height of the font - usually equal to point size
 pub fn font_height(font &Font) int {
 	return C.TTF_FontHeight(font)
 }
 
-// Get the offset from the baseline to the top of the font
-// This is a positive value, relative to the baseline.
 fn C.TTF_FontAscent(font &C.TTF_Font) int
+
+// font_ascent gets the offset from the baseline to the top of the font
+// This is a positive value, relative to the baseline.
 pub fn font_ascent(font &Font) int {
 	return C.TTF_FontAscent(font)
 }
 
-// Get the offset from the baseline to the bottom of the font
-// This is a negative value, relative to the baseline.
 fn C.TTF_FontDescent(font &C.TTF_Font) int
+
+// font_descent gets the offset from the baseline to the bottom of the font
+// This is a negative value, relative to the baseline.
 pub fn font_descent(font &Font) int {
 	return C.TTF_FontDescent(font)
 }
 
-// Get the recommended spacing between lines of text for this font
 fn C.TTF_FontLineSkip(font &C.TTF_Font) int
+
+// font_line_skip gets the recommended spacing between lines of text for this font
 pub fn font_line_skip(font &Font) int {
 	return C.TTF_FontLineSkip(font)
 }
 
-// Get/Set whether or not kerning is allowed for this font
 fn C.TTF_GetFontKerning(font &C.TTF_Font) int
+
+// get_font_kerning gets whether or not kerning is allowed for this font
 pub fn get_font_kerning(font &Font) int {
 	return C.TTF_GetFontKerning(font)
 }
 
 fn C.TTF_SetFontKerning(font &C.TTF_Font, allowed int)
+
+// set_font_kerning sets whether or not kerning is allowed for this font
 pub fn set_font_kerning(font &Font, allowed int) {
 	C.TTF_SetFontKerning(font, allowed)
 }
 
-// Get the number of faces of the font
 fn C.TTF_FontFaces(font &C.TTF_Font) int
+
+// font_faces gets the number of faces of the font
 pub fn font_faces(font &Font) int {
 	return C.TTF_FontFaces(font)
 }
 
-// Get the font face attributes, if any
 fn C.TTF_FontFaceIsFixedWidth(font &C.TTF_Font) int
+
+// font_face_is_fixed_width gets the font face attributes, if any
 pub fn font_face_is_fixed_width(font &Font) int {
 	return C.TTF_FontFaceIsFixedWidth(font)
 }
@@ -181,22 +192,25 @@ pub fn font_face_style_name(font &Font) &char {
 	return C.TTF_FontFaceStyleName(font)
 }
 
-// Check wether a glyph is provided by the font or not
 fn C.TTF_GlyphIsProvided(font &C.TTF_Font, ch u16) int
+
+// glyph_is_provided checks wether a glyph is provided by the font or not
 pub fn glyph_is_provided(font &Font, ch u16) int {
 	return C.TTF_GlyphIsProvided(font, ch)
 }
 
-// Get the metrics (dimensions) of a glyph
+fn C.TTF_GlyphMetrics(font &C.TTF_Font, ch u16, minx &int, maxx &int, miny &int, maxy &int, advance &int) int
+
+// glyph_metrics gets the metrics (dimensions) of a glyph
 // To understand what these metrics mean, here is a useful link:
 // http://freetype.sourceforge.net/freetype2/docs/tutorial/step2.html
-fn C.TTF_GlyphMetrics(font &C.TTF_Font, ch u16, minx &int, maxx &int, miny &int, maxy &int, advance &int) int
 pub fn glyph_metrics(font &Font, ch u16, minx &int, maxx &int, miny &int, maxy &int, advance &int) int {
 	return C.TTF_GlyphMetrics(font, ch, minx, maxx, miny, maxy, advance)
 }
 
-// Get the dimensions of a rendered string of text
 fn C.TTF_SizeText(font &C.TTF_Font, text &char, w &int, h &int) int
+
+// size_text gets the dimensions of a rendered string of text
 pub fn size_text(font &Font, text &char, w &int, h &int) int {
 	return C.TTF_SizeText(font, text, w, h)
 }
@@ -211,12 +225,13 @@ pub fn size_unicode(font &C.TTF_Font, text &u16, w &int, h &int) int {
 	return C.TTF_SizeUNICODE(font, text, w, h)
 }
 
-// Create an 8-bit palettized surface and render the given text at
+fn C.TTF_RenderText_Solid(font &C.TTF_Font, text &char, fg C.SDL_Color) &C.SDL_Surface
+
+// render_text_solid creates an 8-bit palettized surface and render the given text at
 // fast quality with the given font and color.  The 0 pixel is the
 // colorkey, giving a transparent background, and the 1 pixel is set
 // to the text color.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderText_Solid(font &C.TTF_Font, text &char, fg C.SDL_Color) &C.SDL_Surface
 pub fn render_text_solid(font &Font, text string, fg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderText_Solid(font, text.str, fg)
 }
@@ -231,22 +246,24 @@ pub fn render_unicode_solid(font &Font, text &u16, fg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderUNICODE_Solid(font, text, fg)
 }
 
-// Create an 8-bit palettized surface and render the given glyph at
+fn C.TTF_RenderGlyph_Solid(font &C.TTF_Font, ch u16, fg C.SDL_Color) &C.SDL_Surface
+
+// render_glyph_solid creates an 8-bit palettized surface and render the given glyph at
 // fast quality with the given font and color.  The 0 pixel is the
 // colorkey, giving a transparent background, and the 1 pixel is set
 // to the text color.  The glyph is rendered without any padding or
 // centering in the X direction, and aligned normally in the Y direction.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderGlyph_Solid(font &C.TTF_Font, ch u16, fg C.SDL_Color) &C.SDL_Surface
 pub fn render_glyph_solid(font &Font, ch u16, fg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderGlyph_Solid(font, ch, fg)
 }
 
-// Create an 8-bit palettized surface and render the given text at
+fn C.TTF_RenderText_Shaded(font &C.TTF_Font, text &char, fg C.SDL_Color, bg C.SDL_Color) &C.SDL_Surface
+
+// render_text_shaded creates an 8-bit palettized surface and render the given text at
 // high quality with the given font and colors.  The 0 pixel is background,
 // while other pixels have varying degrees of the foreground color.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderText_Shaded(font &C.TTF_Font, text &char, fg C.SDL_Color, bg C.SDL_Color) &C.SDL_Surface
 pub fn render_text_shaded(font &C.TTF_Font, text string, fg sdl.Color, bg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderText_Shaded(font, text.str, fg, bg)
 }
@@ -261,21 +278,23 @@ pub fn render_unicode_shaded(font &Font, text &u16, fg sdl.Color, bg sdl.Color) 
 	return C.TTF_RenderUNICODE_Shaded(font, text, fg, bg)
 }
 
-// Create an 8-bit palettized surface and render the given glyph at
+fn C.TTF_RenderGlyph_Shaded(font &C.TTF_Font, ch u16, fg C.SDL_Color, bg C.SDL_Color) &C.SDL_Surface
+
+// render_glyph_shaded creates an 8-bit palettized surface and render the given glyph at
 // high quality with the given font and colors.  The 0 pixel is background,
 // while other pixels have varying degrees of the foreground color.
 // The glyph is rendered without any padding or centering in the X
 // direction, and aligned normally in the Y direction.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderGlyph_Shaded(font &C.TTF_Font, ch u16, fg C.SDL_Color, bg C.SDL_Color) &C.SDL_Surface
 pub fn render_glyph_shaded(font &Font, ch u16, fg sdl.Color, bg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderGlyph_Shaded(font, ch, fg, bg)
 }
 
-// Create a 32-bit ARGB surface and render the given text at high quality,
+fn C.TTF_RenderText_Blended(font &C.TTF_Font, text &char, fg C.SDL_Color) &C.SDL_Surface
+
+// render_text_blended creates a 32-bit ARGB surface and render the given text at high quality,
 // using alpha blending to dither the font with the given color.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderText_Blended(font &C.TTF_Font, text &char, fg C.SDL_Color) &C.SDL_Surface
 pub fn render_text_blended(font &Font, text &char, fg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderText_Blended(font, text, fg)
 }
@@ -290,12 +309,13 @@ pub fn render_unicode_blended(font &Font, text &u16, fg sdl.Color) &sdl.Surface 
 	return C.TTF_RenderUNICODE_Blended(font, text, fg)
 }
 
-// Create a 32-bit ARGB surface and render the given text at high quality,
+fn C.TTF_RenderText_Blended_Wrapped(font &C.TTF_Font, text &char, fg C.SDL_Color, wrap_length u32) &C.SDL_Surface
+
+// render_text_blended_wrapped creates a 32-bit ARGB surface and render the given text at high quality,
 // using alpha blending to dither the font with the given color.
 // Text is wrapped to multiple lines on line endings and on word boundaries
 // if it extends beyond wrapLength in pixels.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderText_Blended_Wrapped(font &C.TTF_Font, text &char, fg C.SDL_Color, wrap_length u32) &C.SDL_Surface
 pub fn render_text_blended_wrapped(font &Font, text &char, fg sdl.Color, wrap_length u32) &sdl.Surface {
 	return C.TTF_RenderText_Blended_Wrapped(font, text, fg, wrap_length)
 }
@@ -310,36 +330,41 @@ pub fn render_unicode_blended_wrapped(font &Font, text &u16, fg sdl.Color, wrap_
 	return C.TTF_RenderUNICODE_Blended_Wrapped(font, text, fg, wrap_length)
 }
 
-// Create a 32-bit ARGB surface and render the given glyph at high quality,
+fn C.TTF_RenderGlyph_Blended(font &C.TTF_Font, ch u16, fg C.SDL_Color) &C.SDL_Surface
+
+// render_glyph_blended creates a 32-bit ARGB surface and render the given glyph at high quality,
 // using alpha blending to dither the font with the given color.
 // The glyph is rendered without any padding or centering in the X
 // direction, and aligned normally in the Y direction.
 // This function returns the new surface, or NULL if there was an error.
-fn C.TTF_RenderGlyph_Blended(font &C.TTF_Font, ch u16, fg C.SDL_Color) &C.SDL_Surface
 pub fn render_glyph_blended(font &Font, ch u16, fg sdl.Color) &sdl.Surface {
 	return C.TTF_RenderGlyph_Blended(font, ch, fg)
 }
 
-// Close an opened font file
 fn C.TTF_CloseFont(font &C.TTF_Font)
+
+// close_font closes an opened font file
 pub fn close_font(font &Font) {
 	C.TTF_CloseFont(font)
 }
 
-// De-initialize the TTF engine
 fn C.TTF_Quit()
+
+// quit de-initializes the TTF engine
 pub fn quit() {
 	C.TTF_Quit()
 }
 
-// Check if the TTF engine is initialized
 fn C.TTF_WasInit() int
+
+// was_init checks if the TTF engine is initialized
 pub fn was_init() int {
 	return C.TTF_WasInit()
 }
 
-// Get the kerning size of two glyphs
 fn C.TTF_GetFontKerningSizeGlyphs(font &C.TTF_Font, previous_ch u16, ch u16) int
+
+// get_font_kerning_size_glyphs gets the kerning size of two glyphs
 pub fn get_font_kerning_size_glyphs(font &Font, previous_ch u16, ch u16) int {
 	return C.TTF_GetFontKerningSizeGlyphs(font, previous_ch, ch)
 }
