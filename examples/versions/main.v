fb685c99f011d342569c93a9cbaf69328c72428e
Larpon <Larpon@users.noreply.github.com>
2021-12-28 14:59:56 +0100

    all: backport fixes from 2.0.9 (#57)

--- examples/versions/main.v
+++ examples/versions/main.v
@@ -4,7 +4,7 @@ import sdl
 
 fn main() {
 	println('v.mod version $sdl.vmod_version()')
-	println('Const version ${sdl.major_version}.${sdl.minor_version}.${sdl.patchlevel}')
+	println('Const version ${sdl.major_version}.${sdl.minor_version}.$sdl.patchlevel')
 	println('Compiled against version $sdl.version()')
 	println('Runtime loaded version $sdl.get_version()')
 	println('Revision $sdl.get_revision_number() / $sdl.get_revision()')
