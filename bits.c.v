79c9663b0da4e62f3e58ef917fa07f75d826dadd
lmp <dev.larpon@gmail.com>
2021-12-29 11:38:24 +0100

    bits: comment out and add developer notice

--- bits.c.v
+++ bits.c.v
@@ -3,17 +3,20 @@
 // that can be found in the LICENSE file.
 module sdl
 
-
 //
 // SDL_bits.h
 //
+// NOTE not all distros include this file
 
+/*
 fn C.SDL_MostSignificantBitIndex32(x u32) int
+
 // most_significant_bit_index32 gets the index of the most significant bit. Result is undefined when called
 //  with 0. This operation can also be stated as "count leading zeroes" and
 //  "log base 2".
 //
 //  returns index of the most significant bit, or -1 if the value is 0.
-pub fn most_significant_bit_index32(x u32) int{
+pub fn most_significant_bit_index32(x u32) int {
 	return C.SDL_MostSignificantBitIndex32(x)
 }
+*/
