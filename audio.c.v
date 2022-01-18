172e8d7697aa57f994215964fff13ce5982d77c4
Larpon <Larpon@users.noreply.github.com>
2022-01-18 16:14:20 +0100

    all: use V types in V wrapping args and returns (#118)

--- audio.c.v
+++ audio.c.v
@@ -393,7 +393,7 @@ pub fn get_audio_status() AudioStatus {
 
 fn C.SDL_GetAudioStatus() C.SDL_AudioStatus
 
-pub fn get_audio_device_status(dev C.SDL_AudioDeviceID) AudioStatus {
+pub fn get_audio_device_status(dev AudioDeviceID) AudioStatus {
 	return AudioStatus(C.SDL_GetAudioDeviceStatus(C.SDL_AudioDeviceID(dev)))
 }
 
