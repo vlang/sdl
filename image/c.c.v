fe6a395f911ac9ac6c55a5d0fb94a04f0b2b87ba
lmp <dev.larpon@gmail.com>
2022-02-15 16:35:42 +0100

    all: port 2.0.8 Windows additions

--- image/c.c.v
+++ image/c.c.v
@@ -2,8 +2,12 @@ module image
 
 #flag linux -lSDL2_image
 
-#flag windows -I @VMODROOT/thirdparty/SDL2_image/include
-#flag windows -L @VMODROOT/thirdparty/SDL2_image/lib/x64
+$if x64 {
+	#flag windows -L @VMODROOT/thirdparty/SDL2_image-2.0.5/lib/x64
+} $else {
+	#flag windows -L @VMODROOT/thirdparty/SDL2_image-2.0.5/lib/x86
+}
+#flag windows -I @VMODROOT/thirdparty/SDL2_image-2.0.5/include
 #flag windows -lSDL2_image
 
 #include <SDL_image.h>
