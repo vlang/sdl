61db2086248778a21cde407a694bb02e6f67afb3
Larpon <Larpon@users.noreply.github.com>
2022-03-23 16:34:15 +0100

    all: fix doc strings (#267)

--- platform.c.v
+++ platform.c.v
@@ -7,6 +7,8 @@ module sdl
 // SDL_platform.h
 //
 
+fn C.SDL_GetPlatform() &char
+
 // get_platform gets the name of the platform.
 //
 // Here are the names returned for some (but not all) supported platforms:
@@ -23,7 +25,6 @@ module sdl
 // NOTE This function is available since SDL 2.0.0.
 //
 // NOTE the returned &char is const
-fn C.SDL_GetPlatform() &char
 pub fn get_platform() &char {
 	return C.SDL_GetPlatform()
 }
