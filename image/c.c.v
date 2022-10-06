a5a6675cc8bff965d4a9830df61b06ccb57ffada
lmp <dev.larpon@gmail.com>
2022-10-06 13:57:53 +0200

    all: add comptime sdl_no_compile_flags option

--- image/c.c.v
+++ image/c.c.v
@@ -1,6 +1,17 @@
 module image
 
-#flag linux -lSDL2_image
+$if !windows {
+	// SDL libs are loaded dynamically from Java on Android
+	$if !android || termux {
+		// sdl_no_compile_flags allow users to provide
+		// custom flags (e.g. via CFLAGS/LDFLAGS) for the compiler.
+		// This is especially useful when building/linking against a
+		// custom compiled version of the libs on *nix.
+		$if !sdl_no_compile_flags ? {
+			#flag -lSDL2_image
+		}
+	}
+}
 
 $if x64 {
 	#flag windows -L @VMODROOT/thirdparty/SDL2_image-2.0.3/lib/x64
