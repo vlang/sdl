51db75b3bd6ddb294f6a19eb5e65714df496dbfe
lmp <dev.larpon@gmail.com>
2022-10-06 13:57:53 +0200

    all: add comptime sdl_no_compile_flags option

--- mixer/c.c.v
+++ mixer/c.c.v
@@ -3,6 +3,19 @@
 // that can be found in the LICENSE file.
 module mixer
 
+$if !windows {
+	// SDL libs are loaded dynamically from Java on Android
+	$if !android || termux {
+		// sdl_no_compile_flags allow users to provide
+		// custom flags (e.g. via CFLAGS/LDFLAGS) for the compiler.
+		// This is especially useful when building/linking against a
+		// custom compiled version of the libs on *nix.
+		$if !sdl_no_compile_flags ? {
+			#flag -lSDL2_mixer
+		}
+	}
+}
+
 $if x64 {
 	#flag windows -L @VMODROOT/thirdparty/SDL2_mixer-2.0.4/lib/x64
 } $else {
