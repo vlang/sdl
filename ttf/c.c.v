aab047333db29dbb34955e167635d922e8491358
lmp <dev.larpon@gmail.com>
2022-10-06 13:57:53 +0200

    all: add comptime sdl_no_compile_flags option

--- ttf/c.c.v
+++ ttf/c.c.v
@@ -3,6 +3,19 @@
 // that can be found in the LICENSE file.
 module ttf
 
+$if !windows {
+	// SDL libs are loaded dynamically from Java on Android
+	$if !android || termux {
+		// sdl_no_compile_flags allow users to provide
+		// custom flags (e.g. via CFLAGS/LDFLAGS) for the compiler.
+		// This is especially useful when building/linking against a
+		// custom compiled version of the libs on *nix.
+		$if !sdl_no_compile_flags ? {
+			#flag -lSDL2_ttf
+		}
+	}
+}
+
 $if x64 {
 	#flag windows -L @VMODROOT/thirdparty/SDL2_ttf-2.0.14/lib/x64
 } $else {
