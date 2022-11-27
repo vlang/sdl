b93f5f29d57034dcd8102358bad504e22a0fbe4d
Larpon <Larpon@users.noreply.github.com>
2022-11-27 14:16:04 +0100

    examples: use ${} string interpolation (#472)

--- examples/basic_mixer/basic_mixer.v
+++ examples/basic_mixer/basic_mixer.v
@@ -43,9 +43,9 @@ fn (mut pc PlayCycle) next() {
 	if mixer.play_music(music, -1) == -1 {
 		mixer.free_music(music)
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Music sound "$music_key" could not be played: $error_msg')
+		panic('Music sound "${music_key}" could not be played: ${error_msg}')
 	}
-	println('Playing "$music_key"')
+	println('Playing "${music_key}"')
 }
 
 fn (mut pc PlayCycle) previous() {
@@ -63,9 +63,9 @@ fn (mut pc PlayCycle) previous() {
 	if mixer.play_music(music, -1) == -1 {
 		mixer.free_music(music)
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Music sound "$music_key" could not be played: $error_msg')
+		panic('Music sound "${music_key}" could not be played: ${error_msg}')
 	}
-	println('Playing "$music_key"')
+	println('Playing "${music_key}"')
 }
 
 fn get_asset_path(path string) string {
@@ -81,12 +81,12 @@ fn load_wav(path string) !&mixer.Chunk {
 	rw := sdl.rw_from_file(asset_path.str, 'rb'.str)
 	if rw == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		return error('Could not create wav "$path" RW from mem data: $error_msg')
+		return error('Could not create wav "${path}" RW from mem data: ${error_msg}')
 	}
 	wav := mixer.load_wav_rw(rw, 1)
 	if wav == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		return error('Could not load wav RW "$path" data: $error_msg')
+		return error('Could not load wav RW "${path}" data: ${error_msg}')
 	}
 	return wav
 }
@@ -96,12 +96,12 @@ fn load_mus(path string) !&mixer.Music {
 	rw := sdl.rw_from_file(asset_path.str, 'rb'.str)
 	if rw == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		return error('Could not create music "$path" RW from mem data: $error_msg')
+		return error('Could not create music "${path}" RW from mem data: ${error_msg}')
 	}
 	mus := mixer.load_mus_rw(rw, 1)
 	if mus == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		return error('Could not load music RW "$path" data: $error_msg')
+		return error('Could not load music RW "${path}" data: ${error_msg}')
 	}
 	return mus
 }
@@ -113,12 +113,12 @@ fn load_mus(path string) !&mixer.Music {
 // diagnose any potential library problems a setup might have.
 [console]
 fn main() {
-	println('Const version ${mixer.major_version}.${mixer.minor_version}.$mixer.patchlevel')
+	println('Const version ${mixer.major_version}.${mixer.minor_version}.${mixer.patchlevel}')
 	mut compiled_version := sdl.Version{}
 	mixer.mixer_version(&compiled_version)
-	println('Compiled against version $compiled_version.str()')
+	println('Compiled against version ${compiled_version.str()}')
 	linked_version := mixer.linked_version()
-	println('Runtime loaded version ${linked_version.major}.${linked_version.minor}.$linked_version.patch')
+	println('Runtime loaded version ${linked_version.major}.${linked_version.minor}.${linked_version.patch}')
 
 	$if debug ? {
 		// SDL debug info, must be called before sdl.init
@@ -126,26 +126,26 @@ fn main() {
 	}
 	if sdl.init(sdl.init_video | sdl.init_audio) < 0 {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Could not initialize SDL: $error_msg')
+		panic('Could not initialize SDL: ${error_msg}')
 	}
 
 	// Initialize SDL2_mixer
 	if mixer.open_audio(u16(mixer.default_frequency), u16(mixer.default_format), u16(mixer.default_channels),
 		4096) < 0 {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Could not initialize SDL mixer: $error_msg')
+		panic('Could not initialize SDL mixer: ${error_msg}')
 	}
 
 	window := sdl.create_window('Hello SDL2_mixer (Press SPACE to pause/play)'.str, 300,
 		300, 500, 300, 0)
 	if window == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Could not create SDL window: $error_msg')
+		panic('Could not create SDL window: ${error_msg}')
 	}
 	renderer := sdl.create_renderer(window, -1, u32(sdl.RendererFlags.accelerated) | u32(sdl.RendererFlags.presentvsync))
 	if renderer == sdl.null {
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('Could not create SDL renderer: $error_msg')
+		panic('Could not create SDL renderer: ${error_msg}')
 	}
 
 	// Print info to terminal
@@ -161,14 +161,14 @@ Use right/left arrow keys to go to next/previous sound.')
 	if mixer.play_channel(-1, wav, -1) < 0 {
 		mixer.free_chunk(wav)
 		error_msg := unsafe { cstring_to_vstring(sdl.get_error()) }
-		panic('WAV sound could not be played: $error_msg')
+		panic('WAV sound could not be played: ${error_msg}')
 	}
-	println('Playing "$wav_sound" (background loop)')
+	println('Playing "${wav_sound}" (background loop)')
 
 	mut pc := PlayCycle{}
 	for sound in sounds {
 		pc.music[sound] = load_mus(sound) or {
-			eprintln('Loading of file $sound failed: $err.msg()')
+			eprintln('Loading of file ${sound} failed: ${err.msg()}')
 			continue
 		}
 	}
