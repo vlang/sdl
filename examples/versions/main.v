634148a87ace242b85376c99cdec59fe53752e68
Larpon <Larpon@users.noreply.github.com>
2022-11-27 14:09:01 +0100

    examples: use ${} string interpolation (#467)

--- examples/versions/main.v
+++ examples/versions/main.v
@@ -3,13 +3,13 @@ module main
 import sdl
 
 fn main() {
-	println('v.mod version $sdl.vmod_version()')
-	println('Const version ${sdl.major_version}.${sdl.minor_version}.$sdl.patchlevel')
+	println('v.mod version ${sdl.vmod_version()}')
+	println('Const version ${sdl.major_version}.${sdl.minor_version}.${sdl.patchlevel}')
 	mut compiled_version := sdl.Version{}
 	sdl.version(mut compiled_version)
-	println('Compiled against version $compiled_version.str()')
+	println('Compiled against version ${compiled_version.str()}')
 	mut linked_version := sdl.Version{}
 	sdl.get_version(mut linked_version)
-	println('Runtime loaded version $linked_version.str()')
-	println('Revision $sdl.get_revision_number() / $sdl.get_revision()')
+	println('Runtime loaded version ${linked_version.str()}')
+	println('Revision ${sdl.get_revision_number()} / ${sdl.get_revision()}')
 }
