f70e1f86ed171578e52594e3f4c7249b6e469a9a
lmp <dev.larpon@gmail.com>
2025-02-15 13:43:53 +0100

    surface: add missing `C.SDL_Surface` fields

--- surface.c.v
+++ surface.c.v
@@ -51,9 +51,42 @@ pub enum FlipMode {
 	vertical   = C.SDL_FLIP_VERTICAL   // `vertical` flip vertically
 }
 
+// A collection of pixels used in software blitting.
+//
+// Pixels are arranged in memory in rows, with the top row first. Each row
+// occupies an amount of memory given by the pitch (sometimes known as the row
+// stride in non-SDL APIs).
+//
+// Within each row, pixels are arranged from left to right until the width is
+// reached. Each pixel occupies a number of bits appropriate for its format,
+// with most formats representing each pixel as one or more whole bytes (in
+// some indexed formats, instead multiple pixels are packed into each byte),
+// and a byte order given by the format. After encoding all pixels, any
+// remaining bytes to reach the pitch are used as padding to reach a desired
+// alignment, and have undefined contents.
+//
+// When a surface holds YUV format data, the planes are assumed to be
+// contiguous without padding between them, e.g. a 32x32 surface in NV12
+// format with a pitch of 32 would consist of 32x32 bytes of Y plane followed
+// by 32x16 bytes of UV plane.
+//
+// NOTE: This struct is available since SDL 3.2.0.
+//
+// See also: create_surface (SDL_CreateSurface)
+// See also: destroy_surface (SDL_DestroySurface)
 @[noinit; typedef]
 pub struct C.SDL_Surface {
-	// NOTE: Opaque type
+	flags  SurfaceFlags // The flags of the surface, read-only
+	format PixelFormat  // The format of the surface, read-only
+	w      int          // The width of the surface, read-only.
+	h      int          // The height of the surface, read-only.
+	pitch  int          // The distance in bytes between rows of pixels, read-only
+
+	refcount int // Application reference count, used when freeing surface
+
+	reserved voidptr // Reserved for internal use
+pub mut:
+	pixels voidptr // A pointer to the pixels of the surface, the pixels are writeable if non-NULL
 }
 
 pub type Surface = C.SDL_Surface
