b676f78517a08ee4c5ff037c10d45f05e2e6d467
Lartrax <lartrax909@gmail.com>
2026-05-26 12:07:26 +0200

    fix compilation with callbacks on latest V

--- callbacks/on.v
+++ callbacks/on.v
@@ -6,6 +6,11 @@ import sdl
 #postinclude "@VMODROOT/c/sdl_main_use_callbacks_postinclude.h"
 #include     "@VMODROOT/c/sdl_main_use_callbacks_include.h"
 
+fn C.g_sdl_app_init(&voidptr, int, &&char)
+fn C.g_sdl_app_quit(voidptr, sdl.AppResult)
+fn C.g_sdl_app_event(voidptr, &sdl.Event)
+fn C.g_sdl_app_iterate(voidptr)
+
 // on_init will make SDL3 call the passed function callback `f`,
 // when it will call https://wiki.libsdl.org/SDL3/SDL_AppInit .
 pub fn on_init(f sdl.AppInitFunc) {
